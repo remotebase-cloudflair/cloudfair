81b8b562399087952e2a4b31570e8ff8401480cb
abdul-rehman-d <ar618211@gmail.com>
2022-10-14 00:40:30 +0500

    cleanup and tailwind set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [textFromApi, setTextFromApi] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:8080/')
+      .then   (res => console.log(res))
+      .catch  (err => console.log(err))
+      .finally(()  => setLoading(false))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <h1 className="text-2xl font-bold">
+        Hello world!
+      </h1>
     </div>
   );
 }
