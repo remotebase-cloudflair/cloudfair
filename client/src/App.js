e5b64cd4746bd51e563d982e11143b00b9f61852
abdul-rehman-d <ar618211@gmail.com>
2022-10-14 01:13:33 +0500

    integrated api

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,27 @@ const App = () => {
 
   useEffect(() => {
     fetch('http://localhost:8080/')
-      .then   (res => console.log(res))
-      .catch  (err => console.log(err))
+      .then   (res => setTextFromApi(res))
+      .catch  (err => setTextFromApi(err.message))
       .finally(()  => setLoading(false))
   }, [])
 
   return (
-    <div className="w-full h-[100vh] flex justify-center items-center">
-      <h1 className="text-2xl font-bold">
-        Hello world!
-      </h1>
+    <div className="w-full h-[100vh] flex justify-center items-center bg-slate-300">
+      {
+        loading ?
+        <span className="flex h-8 w-8 relative justify-center items-center">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-black opacity-50"></span>
+          <span className="relative inline-flex rounded-full h-6 w-6 bg-black opacity-75"></span>
+        </span>
+        :
+        <h1 className="text-center font-semibold">
+          {textFromApi}
+        </h1>
+      }
+      <div className="w-full fixed bg-slate-500 bottom-0 left-0 text-center text-white text-xs p-2">
+        Made by Cloudfair
+      </div>
     </div>
   );
 }
