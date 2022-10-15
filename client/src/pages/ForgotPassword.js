9301775effdc81272863f212b99dad9e4bf5c17d
Abdul Rehman <abdulrehman.symits@gmail.com>
2022-10-16 04:01:14 +0500

    made forgot password modal

--- client/src/pages/ForgotPassword.js
+++ client/src/pages/ForgotPassword.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputField from '../components/InputField'
+import Modal from '../components/Modal'
+
+import mailIllustration from '../assets/mail.png'
+import { Link } from 'react-router-dom'
 
 function ForgotPassword() {
+  const [ showModal, setShowModal ] = useState(false)
+  const [ email, setEmail ] = useState('')
+
+  const getCensoredEmail = (email) => {
+    const [ mail, domain ] = email.split('@')
+    return mail.slice(0, 2) + Array(mail.length-2).fill('*').join('') + '@' + domain
+  }
+
   return (
     <>
       <div className='flex flex-col flex-grow justify-between w-full py-8 xl:py-16'>
@@ -12,7 +24,8 @@ function ForgotPassword() {
           <h1 className="text-4xl">Forgot Password?</h1>
           <form
             onSubmit={(e) => {
-              e.stopPropagation()
+              e.preventDefault()
+              setShowModal(true)
             }}
             className='space-y-4'
           >
@@ -21,12 +34,29 @@ function ForgotPassword() {
               placeholder='Enter email address'
               name='email'
               type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
             <button className='bg-primary text-white p-2 w-full rounded-md' type="submit">Submit</button>
           </form>
         </div>
       </div>
+      {
+        showModal ?
+          <Modal isOpen={showModal}>
+            <div className='flex flex-col space-y-8 p-16 max-w-md items-center text-center'>
+              <img src={mailIllustration} alt="" className='w-40' />
+              <h2 className='text-4xl'>Done!</h2>
+              <p>Link to set new password has been sent to {getCensoredEmail(email)}</p>
+              <Link className='bg-primary text-white p-2 rounded-md w-64' to='/login'>
+                Open Mail
+              </Link>
+            </div>
+          </Modal>
+        :
+        null
+      }
     </>
   )
 }
