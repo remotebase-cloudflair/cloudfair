4587223d5e404dc4ab0db80742be44ce829ad4dd
danities316 <attayi_316@hotmail.com>
2022-10-15 20:56:42 +0100

    Debug google login sessuin

--- server/server.js
+++ server/server.js
@@ -2,12 +2,46 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import {getGoogleURL} from "./util/getGoogleURL"
+import {OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
+import session from 'express-session';
+import passport from 'passport';
 
 const app = express();
 config();
+var userProfile;
 
 const PORT = process.env.PORT || 8080;
+//express oauth session
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: 'SECRET' 
+  }));
+
+  //passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+  });
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+  });
+
+  /*  Google AUTH  */
+  
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
 
 app.use(cors());
 app.use(express.json());
@@ -17,11 +51,24 @@ app.use(cookieParser())
 app.get("/api", (req,res) => {
     res.send("Hello World! from Api");
 })
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
 
-//Google Outh2 sessions
-// app.get("/api/session", getGoogleURL)
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+
 
 
 app.listen(PORT, ()=> {
     console.log(`Server started on http://localhost:${PORT}/api`);
 })
+
+// https://www.loginradius.com/blog/engineering/google-authentication-with-nodejs-and-passportjs/
