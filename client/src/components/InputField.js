c958391b489d4a262311a80e9d9de11da436fd9e
Abdul Rehman <abdulrehman.symits@gmail.com>
2022-10-16 05:58:28 +0500

    made on boarding page

--- client/src/components/InputField.js
+++ client/src/components/InputField.js
@@ -14,11 +14,41 @@ function InputField({ label, type, ...rest}) {
         {label}
       </label>
       <div className='relative w-full'>
-        <input
-          className='relative border w-full border-lightGray p-2 rounded-md focus-visible:outline-none focus-visible:border-black'
-          type={(type!=='password') ? type : (showPassword) ? 'text' : 'password' }
-          {...rest}
-        />
+        { type === 'select' ?
+          <select
+            className='relative border w-full border-lightGray bg-white p-2 rounded-md focus-visible:outline-none focus-visible:border-black'
+            {...rest}
+          >
+            <option disabled selected value>Select</option>
+            {rest.options.map(op => (
+              <option value={op.value} label={op.label} />
+            ))}
+          </select>
+        : type === 'phonenumber' ?
+          <div className='flex flex-row space-x-4'>
+            <div className='w-20'>
+              <select
+                name="code"
+                className='relative border w-full bg-white border-lightGray p-2 rounded-md focus-visible:outline-none focus-visible:border-black'
+              >
+                <option value='+922' label='+922' />
+              </select>
+            </div>
+            <div className='flex-grow'>
+              <input
+                type='tel'
+                className='relative border w-full border-lightGray p-2 rounded-md focus-visible:outline-none focus-visible:border-black'
+                {...rest}
+              />
+            </div>
+          </div>
+        :
+          <input
+            className='relative border w-full border-lightGray p-2 rounded-md focus-visible:outline-none focus-visible:border-black'
+            type={(type!=='password') ? type : (showPassword) ? 'text' : 'password' }
+            {...rest}
+          />
+        }
         { (type==='password') ?
           <button
             onClick={() => setShowPassword(curr => !curr)}
--- client/src/pages/OnBoarding.js
+++ client/src/pages/OnBoarding.js
@@ -1,10 +1,106 @@
-import React from 'react'
+import { useCallback } from 'react'
+import DropZone from '../components/DropZone'
+import InputField from '../components/InputField'
 
 function OnBoarding() {
+  const onDrop = useCallback(acceptedFiles => {
+    console.log(acceptedFiles);
+  }, [])
+
+  const genderOptions = [
+    {value: 'male', label: 'Male'},
+    {value: 'female', label: 'Female'},
+    {value: 'others', label: 'Others'},
+  ]
+
+  const statusOptions = [
+    {value: 'indiviual', label: 'Indiviual'},
+    {value: 'family', label: 'Family (1-5)'},
+    {value: 'group', label: 'Group (1-5)'},
+    {value: 'agency', label: 'Agency'},
+  ]
+
   return (
-    <div>
-      OnBoardinglorem30
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, id, a consequuntur esse adipisci odit quis dolore animi debitis illo ducimus similique ut eum explicabo. Possimus adipisci hic enim minus sunt mollitia, magni illum reiciendis libero praesentium accusantium dignissimos ratione iste odio porro est labore debitis laboriosam in optio. Dicta eligendi hic, necessitatibus magni velit deleniti expedita aliquid quos corrupti a saepe recusandae sequi sed placeat exercitationem, tempora perferendis ipsa aspernatur rem officiis reiciendis! Quia alias sed fuga quas iure ea incidunt magni similique quibusdam perferendis fugit inventore labore doloribus quod facilis, blanditiis harum unde qui deserunt minima. A, quibusdam.</p>
+    <div className='px-4 md:px-8 lg:px-16 pb-4 md:pb-8 lg:pb-16 pt-2 md:pt-4 lg:pt-8 space-y-4 text-black'>
+      <h1 className='text-5xl'>Let’s start from the basics</h1>
+      <p>First we need to know a few things about you</p>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+        }}
+        className='max-w-5xl 2xl:max-w-7xl mx-auto pt-8 space-y-8'
+      >
+        <div className='w-full flex justify-center items-center'>
+          <DropZone onDrop={onDrop} />
+        </div>
+        <div className='w-full flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-16'>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='address'
+              type='text'
+              label="Address"
+              placeholder='Enter your address'
+              required
+            />
+          </div>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='gender'
+              type='select'
+              label="Gender"
+              placeholder='Select'
+              options={genderOptions}
+              required
+            />
+          </div>
+        </div>
+        <div className='w-full flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-16'>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='nationality'
+              type='text'
+              label="Nationality"
+              placeholder='Enter your nationalty'
+              required
+            />
+          </div>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='status'
+              type='select'
+              label="Status"
+              placeholder='Select'
+              options={statusOptions}
+              required
+            />
+          </div>
+        </div>
+        <div className='w-full flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-16'>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='phonenumber'
+              type='phonenumber'
+              label="Phone Number"
+              placeholder='Enter your phone number'
+              required
+            />
+          </div>
+          <div className='md:basis-1/2'>
+            <InputField
+              name='location'
+              type='text'
+              label="Current Location"
+              placeholder='Enter your current location'
+              required
+            />
+          </div>
+        </div>
+        <div className='w-full flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-16 justify-center'>
+          <button type='submit' className='create-account-link inverted-colors self-center w-full'>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
